942c83ea8cc12fbbf3c7daed2d085d79badbfd83
AxelBadouel <axel.simokengne@gmail.com>
2026-06-12 14:02:26 +0200

    Updated procedure described in metric_tests_for_methods.py used to launch the method tests

--- metric_tests_for_methods.py
+++ metric_tests_for_methods.py
@@ -13,76 +13,65 @@ distance_metrics = ["L2", "dotproduct"]
 
 # The prof said that for the svox dataset, we have to use "gallery" as database and use "queries_night" and "queries_sun" as queries for it
 # This dictionary associates to each database, its particular queries to be used with it
-test_set_db_queries = [
-    ("/content/data/sf_xs/test/database",            "/content/data/sf_xs/test/queries"),
-    ("/content/data/tokyo_xs/test/database",          "/content/data/tokyo_xs/test/queries"),
-    ("/content/data/svox/images/test/gallery",        "/content/data/svox/images/test/queries_night"),
-    ("/content/data/svox/images/test/gallery",        "/content/data/svox/images/test/queries_sun"),
-]
+distance_metrics = f"L2 dotproduct"
+databases = f"/content/data/sf_xs/test/database /content/data/tokyo_xs/test/database /content/data/svox/images/test/gallery /content/data/svox/images/test/gallery"
+queries = f"/content/data/sf_xs/test/queries /content/data/tokyo_xs/test/queries /content/data/svox/images/test/queries_night /content/data/svox/images/test/queries_sun"
+
 # THE PART FOR  EVERYONE ENDS HERE
+#   UNCOMMENT AND PASTE IN COLAB IF YOU WANT TO USE NETVLAD
+"""
+!python /content/Visual-Place-Recognition-Project/VPR-methods-evaluation/main.py \
+--num_workers 8 \
+--batch_size 32 \
+--log_dir log_dir \
+--method=netvlad --backbone=VGG16 --descriptors_dimension=4096 \
+--image_size 480 640 \
+--database_folder $databases \
+--queries_folder $queries \
+--num_preds_to_save 20 \
+--recall_values 20 \
+--distance_metric $distance_metrics
+"""
+#   UNCOMMENT AND PASTE IN COLAB IF YOU WANT TO USE COSPLACE
+"""
+!python /content/Visual-Place-Recognition-Project/VPR-methods-evaluation/main.py \
+--num_workers 8 \
+--batch_size 32 \
+--log_dir log_dir \
+--method=cosplace --backbone=ResNet18 --descriptors_dimension=512 \
+--image_size 512 512 \
+--database_folder $database \
+--queries_folder $queries \
+--num_preds_to_save 20 \
+--recall_values 20 \
+--distance_metric=$distance_metric
+"""
+#   UNCOMMENT AND PASTE IN COLAB IF YOU WANT TO USE MIXVPR
+"""
+!python /content/Visual-Place-Recognition-Project/VPR-methods-evaluation/main.py \
+--num_workers 8 \
+--batch_size 32 \
+--log_dir log_dir \
+--method=mixvpr --backbone=ResNet50 --descriptors_dimension=512 \
+--image_size 320 320 \
+--database_folder $database \
+--queries_folder $queries \
+--num_preds_to_save 20 \
+--recall_values 20 \
+--distance_metric=$distance_metric
+"""
 
-for distance_metric in distance_metrics:
-  for database, queries in test_set_db_queries:
-    #   UNCOMMENT AND PASTE IN COLAB IF YOU WANT TO USE NETVLAD
-    """
-    !python /content/Visual-Place-Recognition-Project/VPR-methods-evaluation/main.py \
-    --num_workers 8 \
-    --batch_size 32 \
-    --log_dir log_dir \
-    --method=netvlad --backbone=VGG16 --descriptors_dimension=4096 \
-    --image_size 480 640 \
-    --database_folder $database \
-    --queries_folder $queries \
-    --num_preds_to_save 20 \
-    --recall_values 20 \
-    --distance_metric=$distance_metric
-    """
-for distance_metric in distance_metrics:
-  for database, queries in test_set_db_queries:
-    #   UNCOMMENT AND PASTE IN COLAB IF YOU WANT TO USE COSPLACE
-    """
-    !python /content/Visual-Place-Recognition-Project/VPR-methods-evaluation/main.py \
-    --num_workers 8 \
-    --batch_size 32 \
-    --log_dir log_dir \
-    --method=cosplace --backbone=ResNet18 --descriptors_dimension=512 \
-    --image_size 512 512 \
-    --database_folder $database \
-    --queries_folder $queries \
-    --num_preds_to_save 20 \
-    --recall_values 20 \
-    --distance_metric=$distance_metric
-    """
-for distance_metric in distance_metrics:
-  for database, queries in test_set_db_queries:
-    #   UNCOMMENT AND PASTE IN COLAB IF YOU WANT TO USE MIXVPR
-    """
-    !python /content/Visual-Place-Recognition-Project/VPR-methods-evaluation/main.py \
-    --num_workers 8 \
-    --batch_size 32 \
-    --log_dir log_dir \
-    --method=mixvpr --backbone=ResNet50 --descriptors_dimension=512 \
-    --image_size 320 320 \
-    --database_folder $database \
-    --queries_folder $queries \
-    --num_preds_to_save 20 \
-    --recall_values 20 \
-    --distance_metric=$distance_metric
-    """
-
-for distance_metric in distance_metrics:
-  for database, queries in test_set_db_queries:
-    #   UNCOMMENT AND PASTE IN COLAB IF YOU WANT TO USE MEGALOC
-    """
-    !python /content/Visual-Place-Recognition-Project/VPR-methods-evaluation/main.py \
-    --num_workers 8 \
-    --batch_size 32 \
-    --log_dir log_dir \
-    --method=megaloc --backbone=Dinov2 --descriptors_dimension=8448 \
-    --image_size 322 322 \
-    --database_folder $database \
-    --queries_folder $queries \
-    --num_preds_to_save 20 \
-    --recall_values 20 \
-    --distance_metric=$distance_metric
-    """
+#   UNCOMMENT AND PASTE IN COLAB IF YOU WANT TO USE MEGALOC
+"""
+!python /content/Visual-Place-Recognition-Project/VPR-methods-evaluation/main.py \
+--num_workers 8 \
+--batch_size 32 \
+--log_dir log_dir \
+--method=megaloc --backbone=Dinov2 --descriptors_dimension=8448 \
+--image_size 322 322 \
+--database_folder $database \
+--queries_folder $queries \
+--num_preds_to_save 20 \
+--recall_values 20 \
+--distance_metric=$distance_metric
+"""
